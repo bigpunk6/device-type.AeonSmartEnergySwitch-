d312cb217fbe50233e2364880ad5087a5e0a0167
bigpunk6 <mkurtzjr@live.com>
2015-02-03 14:48:39 -0500

    Update aeon-smart-enerty-switch.device-type.groovy

--- aeon-smart-enerty-switch.device-type.groovy
+++ aeon-smart-enerty-switch.device-type.groovy
@@ -3,9 +3,9 @@
  *
  * Reference from Aeon_HEMv2.groovy by Barry A. Burke "https://github.com/SANdood/Aeon-HEM-v2"
  */
+
 metadata {
-	// Automatically generated. Make future change here.
-	definition (name: "Aeon Smart Energy Switch w/Cost", author: "Mike") {
+	definition (name: "Aeon Smart Energy Switch w/Cost", author: "bigpunk6") {
 		capability "Energy Meter"
         capability "Actuator"
         capability "Switch"
@@ -13,15 +13,16 @@ metadata {
         capability "Polling"
         capability "Refresh"
         capability "Sensor"
-
-        attribute "energyCost", "string" 
-
-		command "reset"
+        capability "Configuration"
+        
+        attribute "energyCost", "string"
+        
+        command "reset"
+        command "configure"
         
 		fingerprint deviceId: "0x2101", inClusters: " 0x70,0x31,0x72,0x86,0x32,0x80,0x85,0x60"
 	}
 
-	// simulator metadata
     simulator {
 	    status "on":  "command: 2003, payload: FF"
         status "off": "command: 2003, payload: 00"
@@ -40,7 +41,6 @@ metadata {
         reply "200100,delay 100,2502": "command: 2503, payload: 00"
 	}
 
-	// tile definitions
 	tiles {
 		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
             state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
@@ -64,14 +64,15 @@ metadata {
 
 		main (["switch","power","energy","energyCost"])
 		details(["switch","power","energy","energyCost","reset","refresh","configure"])
-	        }
-                preferences {
-                     input "kWhCost", "string", title: "\$/kWh (0.16)", defaultValue: "0.16" as String
-                }
+	}
+    
+    preferences {
+        input "kWhCost", "string", title: "\$/kWh (0.16)", defaultValue: "0.16" as String
+    }
 }
 
 def parse(String description) {
-//	log.debug "Parse received ${description}"
+    log.debug "Parse received ${description}"
 	def result = null
 	def cmd = zwave.parse(description, [0x31: 1, 0x32: 1, 0x60: 3])
 	if (cmd) {
@@ -80,62 +81,45 @@ def parse(String description) {
 	if (result) { 
 		log.debug "Parse returned ${result?.descriptionText}"
 		return result
-	} else {
+    } else {
 	}
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.meterv1.MeterReport cmd) {
 
+def zwaveEvent(physicalgraph.zwave.commands.meterv1.MeterReport cmd) {
+    log.debug cmd
     def dispValue
     def newValue
     def formattedValue
-
-        if (cmd.scale == 0) {
+    
+	def timeString = new Date().format("h:mm a", location.timeZone)
+    
+    if (cmd.meterType == 33) {
+		if (cmd.scale == 0) {
         	newValue = Math.round(cmd.scaledMeterValue * 100) / 100
         	if (newValue != state.energyValue) {
         		formattedValue = String.format("%5.2f", newValue)
     			dispValue = "${formattedValue}\nkWh"
-                sendEvent(name: "energy", value: dispValue as String, unit: "", descriptionText: "Display Energy: ${newValue} kVh")
                 state.energyValue = newValue
                 BigDecimal costDecimal = newValue * ( kWhCost as BigDecimal )
                 def costDisplay = String.format("%5.2f",costDecimal)
+                log.debug costDisplay
                 state.costDisp = "Cost\n\$"+costDisplay
-                if (state.display == 1) { sendEvent(name: "energyCost", value: state.costDisp, unit: "", descriptionText: "Display Cost: \$${costDisp}") }
-                [name: "energy", value: newValue, unit: "kWh", descriptionText: "Total Energy: ${formattedValue} kWh"]
+                log.debug costDisp
+                log.debug state.costDisp
+                sendEvent(name: "energyCost", value: state.costDisp, unit: "", descriptionText: "Display Cost: ${costDisplay}")
+                sendEvent(name: "energy", value: newValue, unit: "kWh", descriptionText: "Total Energy: ${formattedValue} kWh")
             }
-        }
-		else if (cmd.scale == 1) {
-            newValue = Math.round( cmd.scaledMeterValue * 100) / 100
-            if (newValue != state.energyValue) {
-            	formattedValue = String.format("%5.2f", newValue)
-    			dispValue = "${formattedValue}\nkVAh"
-                sendEvent(name: "energy", value: dispValue as String, unit: "", descriptionText: "Display Energy: ${formattedValue} kVAh")
-                state.energyValue = newValue
-				[name: "energy", value: newValue, unit: "kVAh", descriptionText: "Total Energy: ${formattedValue} kVAh"]
-            }
-        }
+		}
 		else if (cmd.scale==2) {				
         	newValue = Math.round(cmd.scaledMeterValue)		// really not worth the hassle to show decimals for Watts
         	if (newValue != state.powerValue) {
     			dispValue = newValue+"\nWatts"
-                sendEvent(name: "power", value: dispValue as String, unit: "", descriptionText: "Display Power: ${newValue} Watts")
-                
-                if (newValue < state.powerLow) {
-                	dispValue = newValue+"\n"+timeString
-                	if (state.display == 1) { sendEvent(name: "powerOne", value: dispValue as String, unit: "", descriptionText: "Lowest Power: ${newValue} Watts")	}
-                    state.powerLow = newValue
-                    state.powerLowDisp = dispValue
-                }
-                if (newValue > state.powerHigh) {
-                	dispValue = newValue+"\n"+timeString
-                	if (state.display == 1) { sendEvent(name: "powerTwo", value: dispValue as String, unit: "", descriptionText: "Highest Power: ${newValue} Watts")	}
-                    state.powerHigh = newValue
-                    state.powerHighDisp = dispValue
-                }
                 state.powerValue = newValue
-                [name: "power", value: newValue, unit: "W", descriptionText: "Total Power: ${newValue} Watts"]
+                sendEvent(name: "power", value: newValue, unit: "W", descriptionText: "Total Power: ${newValue} Watts")
             }
-        }
+		}
+ 	}          
 }
 
 def zwaveEvent(physicalgraph.zwave.Command cmd) {
